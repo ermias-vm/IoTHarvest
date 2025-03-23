3b5ff6dfe7971f684da7f6f9273c3bd9b3829698
marc.de.rialp <marc.de.rialp@estudiantat.upc.edu>
2025-03-23 21:02:59 +0100

    New frontend version

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -5,28 +5,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Datos de Sensores</h1>
-      <table border={1} style={{ width: "100%", textAlign: "center" }}>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Temperatura (°C)</th>
-            <th>Humedad (%)</th>
-            <th>Fecha</th>
-          </tr>
-        </thead>
-        <tbody>
-          {mockData.map((data) => (
-            <tr key={data.id}>
-              <td>{data.id}</td>
-              <td>{data.temperatura}</td>
-              <td>{data.humedad}</td>
-              <td>{data.fecha}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="background-container">
+      <div className="title">
+        <h1>IoT Harvest</h1>
+      </div>
+      <div className="graphs-container">
+      </div>
+      <div className="photo-container">
+      </div>
     </div>
   );
 }
