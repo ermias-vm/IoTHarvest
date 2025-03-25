8bee4eba3ccf60361203dac296bfb61c8395ffb7
marc.de.rialp <marc.de.rialp@estudiantat.upc.edu>
2025-03-25 11:53:28 +0100

    New frontend version

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -1,4 +1,3 @@
-// src/App.js
 import React from 'react';
 import mockData from './mockData';
 import './App.css';
@@ -10,6 +9,26 @@ function App() {
         <h1>IoT Harvest</h1>
       </div>
       <div className="graphs-container">
+        <div className="air-humidity-container">
+          <div className="air-humidity-title">
+            <h1>Air Humidity</h1>
+          </div>
+        </div>
+        <div className="ground-humidity-container">
+          <div className="ground-humidity-title">
+            <h1>Ground Humidity</h1>
+          </div>
+        </div>
+        <div className="temperature-container">
+          <div className="temperature-title">
+            <h1>Temperature</h1>
+          </div>
+        </div>
+        <div className="real-time-container">
+          <div className="real-time-title">
+            <h1>Real Time</h1>
+          </div>
+        </div>
       </div>
       <div className="photo-container">
       </div>
